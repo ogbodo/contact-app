1a98e2ac821f213157db93f919857b529392364c
Izuking <matthiasogbodo@gmail.com>
2019-07-17 12:49:07 +0100

    minor changes

--- src/schemas/contact-schema.ts
+++ src/schemas/contact-schema.ts
@@ -3,8 +3,16 @@ import joi from '@hapi/joi';
 const namePattern = /\b[a-zA-Z]+\b$/;
 const phone = /^(\+[0-9]{3}|0)[0-9]{10}$/;
 
-export const optionals = {
+export const post = {
   title: joi.string().optional(),
+  fullName: joi
+    .string()
+    .required()
+    .regex(namePattern),
+  phone: joi
+    .string()
+    .regex(phone)
+    .required(),
   mobile: joi
     .string()
     .regex(phone)
@@ -21,16 +29,34 @@ export const optionals = {
   zipCode: joi.string().optional(),
   website: joi.string().optional(),
 };
-export const required = {
+
+export const patch = {
+  title: joi.string().optional(),
   fullName: joi
     .string()
-    .required()
+    .optional()
     .regex(namePattern),
   phone: joi
     .string()
     .regex(phone)
-    .required(),
+    .optional(),
+  mobile: joi
+    .string()
+    .regex(phone)
+    .optional(),
+  email: joi
+    .string()
+    .email()
+    .optional(),
+  homeAddress: joi.string().optional(),
+  company: joi.string().optional(),
+  country: joi.string().optional(),
+  state: joi.string().optional(),
+  street: joi.string().optional(),
+  zipCode: joi.string().optional(),
+  website: joi.string().optional(),
 };
-export const iDSchema = {
+
+export const id = {
   id: joi.string().required(),
 };
